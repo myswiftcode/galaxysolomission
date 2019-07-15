f14e81ae6ea49fa9d2ee8aa165cf9310c3f5492b
Vasily Sergeev <v.sergeev.m@gmail.com>
2019-07-15 03:49:16 -0300

    Add files via upload

---
+++ Galaxy Solo Mission/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Galaxy Solo Mission
+//
+//  Created by V.Sergeev on 15/07/2019.
+//  Copyright © 2019 v.sergeev.m@icloud.com. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+        }
+    }
